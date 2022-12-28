c3fd63fb2e3de48f20d8a2903458c6ec1d7a26c3
Justin CZK <justinchiengzeekhai@gmail.com>
2022-12-28 11:06:31 +0800

    complete

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+    session_start();
+    
+    //make sure the user is logged in
+    if ( isset ($_SESSION['user'] ) ){
+        //delete the user's session data
+        unset ($_SESSION['user']);
+        //redirect user back to index
+        header('location:/');
+        exit;
+    }else{
+        //redirect to login page if user is not logged in
+        header('location:/');
+        exit;
+    }
+
+
+?>
